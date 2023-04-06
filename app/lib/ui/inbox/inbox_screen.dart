5aa5044db50fa3ae0273b7e13c94e60aeaa55a5c
ibnuhafizh <hafizhputraa@gmail.com>
2023-04-06 16:30:10 +0700

    add inbox screen UI

---
+++ app/lib/ui/inbox/inbox_screen.dart
@@ -0,0 +1,31 @@
+// ignore: implementation_imports
+import 'package:app/ui/compose_mail/compose_mail_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+// ignore: implementation_imports
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class InboxScreen extends StatelessWidget {
+  const InboxScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inbox'),
+      ),
+      body: Center(
+        child: Text('Inbox Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ComposeEmailScreen()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
